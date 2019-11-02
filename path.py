02673f1c2f044c20c50a44ad28de7a2db5632a1c
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-11-02 03:22:03 +0100

    Add files via upload

---
+++ path.py
@@ -0,0 +1,5 @@
+wall_no_pieces = "pictures/wall/wall_no_pieces/"
+wall_pieces = "pictures/wall/wall_pieces/"
+wall_save = "pictures/wall/save_wall_diff/"
+save_parameters_wall_top = "info_data/trackbaretop.py"
+save_parameters_wall_bot = "info_data/trackbarebot.py"
