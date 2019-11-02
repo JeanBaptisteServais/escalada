69de64c817d6bed60320668b72872e482140a656
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-11-02 17:09:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import os
 import cv2
+"""
+-        On this picture, we have two backgrounds. The bottom of the
+    picture and the top of the picture. It is because the wall have 2
+    differents colors. The blue and the green.
+
+-        We try to delete the background  and recup only the holds
+    by the wall_parameters parameters.
+
+    For this we are based on contours. We delete the big and litlle
+    contours for the top side.
+
+    For the bottom we recup the max contour who's moutains pics of
+    the picture and re-operate the filtering of contours.
+
+"""
+
+
+
 import numpy as np
 
 from pictures_function import open_picture
 from pictures_function import show_picture
 from pictures_function import blanck_picture
-
-from path import save_parameters_wall_bot as para_bot
 
 
 def first_operation_picture(path_picture):
@@ -65,32 +81,182 @@ def HSV_mask(img, parameter1, parameter2):
     return mask
 
 
-def draw_contours(mask, blanck, R, P, copy, position):
-    
-    contours, _ = cv2.findContours(mask, R, P)
+def delimitate_contours(cnts, minimum, maximum, blanck):
+
+    #Delete litle and big contours.
+    if cv2.contourArea(cnts) > minimum and\
+       cv2.contourArea(cnts) < maximum:
+
+        #Recuperate area of detections.
+        cv2.drawContours(blanck,[cnts],-1,(255,255,255), 1)
+        (x,y,w,h) = cv2.boundingRect(cnts)
+
+        return (x, y, w, h)
+
+
+
+def recup_top_points(blanck, copy, pts, position):
+    """Here we draw contours for eliminate noise
+    or massive area of pixels like light."""
+
+    #Convert tuple position into variables
+    x=pts[0]; y=pts[1]; w=pts[2]; h=pts[3];
+
+    #the detection isn't a noise.
+    if w+h > 15:
+
+        #print(w+h)
+        #Display detection on the picture.
+        cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 0, 255), 3)
+
+        #Recuperate points in case.
+        position.append(pts)
+
+        #show_picture("blanck", blanck, 0, "")
+        #show_picture("copy", copy, 0, "")
+
+    return copy
+
+
+def recup_bot_points(blanck, copy, pts, position):
+
+    #Convert tuple position into variables
+    x=pts[0]; y=pts[1]; w=pts[2]; h=pts[3];
+
+    #the detection isn't a noise (a chain in this case).
+    if h < 50:
+
+        #print(h)
+        #Display detection on the picture.
+        cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 0, 255), 3)
+
+        #Recuperate points in case.
+        position.append(pts)
+
+        #show_picture("copy", copy, 0, "")
+        #show_picture("blanck_bot", blanck_bot, 0, "")
+
+    return copy
+
+
+def recuperate_max_contours(contours):
+    """Here we recuperate the maximum contours.
+    If contour is superior of the current contour
+    make this contours the max.
+    """
 
     maxi = 0
     for cnt in contours:
         if cv2.contourArea(cnt) > maxi:
             maxi = cv2.contourArea(cnt)
+  
+    return maxi
+
+
+
+def draw_contours_bot(contours, blanck, maxi):
+    """We recuperate all contours into the max contour"""
+
 
     for cnts in contours:
-        if cv2.contourArea(cnts) > 10 and\
-           cv2.contourArea(cnts) < 1000:
+        #The current contour is the max contour
+        if cv2.contourArea(cnts) == maxi:
 
+            #We draw it.
             cv2.drawContours(blanck,[cnts],-1,(255,255,255), 1)
-            #cv2.fillPoly(blanck, pts =[cnts], color=(255, 255, 255))
+            #We recuperate his area.
             (x,y,w,h) = cv2.boundingRect(cnts)
-            position.append([x, y, w, h])
 
-            print("ici", w, h)
-            cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 0, 255), 3)
+        else:
+            #Draw others contours into the max contours.
+            cv2.drawContours(blanck,[cnts],-1,(255,255,255), 1)
+            cv2.fillPoly(blanck, pts =[cnts], color=(255, 255, 255))
 
-            #show_picture("blanck", blanck, 0, "")
-            #show_picture("copy", copy, 0, "")
+    #Make the rest (outside max contour) black.
+    blanck[0:y, x:x+w] = 0
+
+    #show_picture("blanck", blanck, 0, "")
+
+    return blanck
 
 
-    return copy, blanck
+
+def top_side_picture(img, copy, position):
+    """Here we recuperate, delete and display
+    our detections from the top of the picture"""
+
+    #Recup parameters from wall_parameters.
+    from path import save_parameters_wall_top as para_top
+    parameters = recup_parameters_HSV(para_top)
+    #print(parameters)
+
+    #Make a mask for delete background.
+    mask = HSV_mask(img, parameters[:3], parameters[3:])
+    #show_picture("mask", mask, 0, "y")
+
+    #Create new picture.
+    blanck = blanck_picture(img)
+
+    #Recuperate contours.
+    contours, _ = cv2.findContours(mask, R, P)
+    for cnts in contours:
+
+        #Draw contours, estimate them, delete noise and recup pieces.
+        pts = delimitate_contours(cnts, 10, 1000, blanck)
+        #print(pts)
+
+        if pts:
+            #We displaying the detections and recuperate points.
+            copy = recup_top_points(blanck, copy, pts, position)
+
+            show_picture("detections", copy, 0, "")
+
+
+
+def bot_side_picture(img, copy, position):
+    """Here we recuperate, delete and display
+    our detections of the bottom of the picture"""
+ 
+    #Recuperate parameters from wall parameters bot.
+    from path import save_parameters_wall_bot as para_bot
+    parameters = recup_parameters_HSV(para_bot)
+    #print(parameters)
+
+    #Create a HSV mask.
+    mask = HSV_mask(img, parameters[:3], parameters[3:])
+    #show_picture("mask", mask, 0, "y")
+
+    #Create new black picture.
+    blanck = blanck_picture(img)
+
+    #Recuperate contours.
+    contours, _ = cv2.findContours(mask, R, P)
+
+    #Recuperate the max contour.
+    maxi = recuperate_max_contours(contours)
+    blanck = draw_contours_bot(contours, blanck, maxi)
+
+    #Filter the blanck picture.
+    gray = cv2.cvtColor(blanck, cv2.COLOR_BGR2GRAY)
+
+    #Recuperate contours from the max contour.
+    contours, _ = cv2.findContours(gray, R, P)
+
+    #Create new black picture.
+    blanck_bot = blanck_picture(img)
+
+    #Recuperate contours on the max contour.
+    for cnts in contours:
+
+        #Filter the noise and no interesting contours.
+        pts = delimitate_contours(cnts, 200, 10000, blanck_bot)
+
+        if pts:
+            #Recuperate position and display it if points exists.
+            copy = recup_bot_points(blanck, copy, pts, position)
+
+            show_picture("detections", copy, 0, "")
+
 
 
 
@@ -114,161 +280,32 @@ if __name__ == "__main__":
 
     for i in liste_wall_pieces:
 
+
         #Resize + delete ground and roof
         img = first_operation_picture(wp + i)
 
         #Make a copy of original
         copy = img.copy()
 
-        #Recup parameters from wall_parameters
-        from path import save_parameters_wall_top as para_top
-        parameters = recup_parameters_HSV(para_top)
-        #print(parameters)
+        """---Top of picture---"""
 
-        #Make a mask for delete background
-        mask = HSV_mask(img, parameters[:3], parameters[3:])
-        show_picture("mask", mask, 0, "")
+        print("TOPSIDE")
 
-        #Create new picture
-        blanck = blanck_picture(img)
+        top_side_picture(img, copy, position)
 
-        #Draw contours for delete noise and recup pieces.
-        copy, blanck =\
-        draw_contours(mask, blanck, R, P, copy, position)
 
-        show_picture("copy", copy, 0, "")
-        show_picture("blanck", blanck, 0, "")
+        """---Bot of picture---"""
 
+        print("BOTSIDE")
 
+        bot_side_picture(img, copy, position)
 
+        #Here are position of pieces.
+        print("All our detection are :\n\n")
+        for pos in position:
+            print(pos)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #bas
-        l_b1 = np.array([76, 47, 0])
-        u_b1 = np.array([98, 192, 255])
-        mask1 = cv2.inRange(hsv, l_b1, u_b1)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        #show_picture("mask1", mask1, 0, "")
-        
-
-
-
-        blanck = blanck_picture(img);
-
-        contours, _ = cv2.findContours(mask1, R, P)
-
-        maxi = 0
-        for cnt in contours:
-            if cv2.contourArea(cnt) > maxi:
-                maxi = cv2.contourArea(cnt)
-
-
-        for cnts in contours:
-
-            if cv2.contourArea(cnts) == maxi:
-                cv2.drawContours(blanck,[cnts],-1,(255,255,255), 1)
-                #cv2.fillPoly(blanck, pts =[cnts], color=(255, 255, 255))
-                (x,y,w,h) = cv2.boundingRect(cnts)
-            else:
-                cv2.drawContours(blanck,[cnts],-1,(255,255,255), 1)
-                cv2.fillPoly(blanck, pts =[cnts], color=(255, 255, 255))
-
-        
-
-        blanck[0:y, x:x+w] = 0
-
-        show_picture("blanck", blanck, 0, "")
-
-
-
-
-        gray = cv2.cvtColor(blanck, cv2.COLOR_BGR2GRAY)
-        contours, _ = cv2.findContours(gray, R, P)
-        blanck_bot = blanck_picture(img);
-
-        for cnts in contours:
-            if cv2.contourArea(cnts) > 200 and\
-               cv2.contourArea(cnts) < 10000:
-
-                cv2.drawContours(blanck_bot,[cnts],-1,(255,255,255), 1)
-
-                (x,y,w,h) = cv2.boundingRect(cnts)
-                if h < 50:
-                    print(cv2.contourArea(cnts), w,h)
-                    show_picture("blanck_bot", blanck_bot, 0, "")
-
-                    try:
-                        crop_mask = blanck[y-5:y+h+5, x-5:x+w+5]
-                        show_picture("crop_mask", crop_mask, 0, "")
-                    except:
-                        pass
-
-
-                    cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 0, 255), 3)
-                    show_picture("copy", copy, 0, "")
-
-
-                    crop = img[y-5:y+h+5, x-5:x+w+5]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print("")
 
 
 
